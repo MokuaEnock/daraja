bae2aa065d5d7056a7cd8710fe7f369a3f75682a
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 02:29:27 +0300

    add request

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 
     let headers = new Headers();
     headers.append("Content-Type", "application/json");
-    headers.append("Authorization", "Bearer ZbZpvsIl2FTYxNYeGoRUBt6oAPmy");
+    headers.append("Authorization", "Bearer IJPe4iABdZQ0DH9uAK2pIfnOUqUU");
 
     fetch("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest", {
       method: "POST",
@@ -35,14 +35,14 @@ function App() {
       body: JSON.stringify({
         BusinessShortCode: 174379,
         Password:
-          "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjIxMjI5MDIxMzUx",
-        Timestamp: "20221229021351",
+          "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjIxMjI5MDIyNTIw",
+        Timestamp: "20221229022520",
         TransactionType: "CustomerPayBillOnline",
         Amount: 1,
-        PartyA: 254708374149,
+        PartyA: 254717594017,
         PartyB: 174379,
-        PhoneNumber: 254708374149,
-        CallBackURL: "https://mydomain.com/path",
+        PhoneNumber: 254717594017,
+        CallBackURL: "https://daraja.vercel.app/",
         AccountReference: "CompanyXLTD",
         TransactionDesc: "Payment of X",
       }),
