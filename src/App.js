1fe0ce19924cd1b15e12c7f8fbaa2d49cf63396a
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 01:38:44 +0300

    grab user inputs from inputs

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import { useState } from "react";
 function App() {
   let [phone, setPhone] = useState("");
   let [amount, setAmount] = useState("");
-  
+
   function handleSubmit(e) {
     e.preventDefault();
   }
   return (
     <main>
       <form onSubmit={handleSubmit}>
-        <input type="number" placeholder="Phone" />
-        <input type="number" placeholder="Total" />
+        <input
+          type="number"
+          placeholder="Phone"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Total"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
     </main>
