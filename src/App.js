021c2d32bd336c0f8a8e573f53a77764f977a422
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 15:42:39 +0300

    reconfigure default routes and compoents

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import Login from "./Login";
 import Signup from "./Signup";
 import Landing from "./Landing";
+import Main from "./Main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={Login} path="/login" />
-        <Route element={Landing} path="/landing" />
-        <Route element={Signup} path="/signup" />
+        <Route element={<Login />} path="/login" />
+        <Route element={<Main />} path="/" />
+        <Route element={<Landing />} path="/landing" />
+        <Route element={<Signup />} path="/signup" />
       </Routes>
     </BrowserRouter>
   );
