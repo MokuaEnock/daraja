4c61e2e454e58a74af2276fa516390165e72438a
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 15:20:38 +0300

    create sigup with google button

--- src/App.js
+++ src/App.js
@@ -1,44 +1,7 @@
 import "./App.css";
-import { useState } from "react";
 
 function App() {
-  
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    // const date = new Date();
-    // const year = date.getFullYear();
-    // const month = date.getMonth() + 1; // Returns a zero-based month (0-11)
-    // const day = date.getDate();
-    // const hour = date.getHours();
-    // const minutes = date.getMinutes();
-    // const seconds = date.getSeconds();
-
-    // function secss(x) {
-    //   if (x < 10) {
-    //     return `0${x}`;
-    //   } else {
-    //     return `${x}`;
-    //   }
-    // }
-    // let timestamped = `${year}${month}${day}${hour}${minutes}${secss(seconds)}`;
-    // console.log(phone, amount, timestamped);
-
-    // let short_code = 174379;
-    // let passkey =
-    //   "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
-    // let party_a = 254717594017;
-    // let account_ref = 2255;
-    // let sty = `${short_code}${passkey}${timestamped}`;
-    // let password = btoa(sty);
-
-  }
-
-  return (
-    <main>
-
-    </main>
-  );
+  return <main></main>;
 }
 
 export default App;
---
+++ src/Signup.js
@@ -0,0 +1,14 @@
+export default function Signup() {
+  return (
+    <section>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <button type="submit">Submit</button>
+      </form>
+      <span>Signup With Google</span>
+    </section>
+  );
+}
