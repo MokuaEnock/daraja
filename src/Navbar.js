5d28af145a486e59c874ffbdf71d6b2b56bcbc38
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 16:31:04 +0300

    create Navbar llinks

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,11 @@
+import { NavLink } from "react-router-dom";
+
 export default function Navbar() {
-  return <header>
-    
-  </header>;
+  return (
+    <header>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/signup">Signup</NavLink>
+    </header>
+  );
 }
