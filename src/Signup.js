4dfe1508c852fa76cd49c8988d892cb11dfa224a
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 16:52:11 +0300

    add placeholder text

--- src/Signup.js
+++ src/Signup.js
@@ -2,10 +2,9 @@ export default function Signup() {
   return (
     <main>
       <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Enter Password" />
+        <input type="text" placeholder="Confirm Password" />
         <button type="submit">Submit</button>
       </form>
       <span>Signup With Google</span>
