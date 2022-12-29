0cb73f1649922624c97b430345099d67394db738
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 15:27:04 +0300

    import routes from react router dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Login from "./login";
 import Signup from "./Signup";
 import Landing from "./Landing";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return <main></main>;
