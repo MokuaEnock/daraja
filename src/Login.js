438d859493704cf0c45fef6003bfc49d27384a60
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 15:59:56 +0300

    set state for email and password

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Login() {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
   return (
     <main>
       <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
+        <input type="text" placeholder="Email" />
+        <input type="email" placeholder="Password" />
 
         <button type="submit">Log In</button>
       </form>
