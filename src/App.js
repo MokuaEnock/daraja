fdeb40637bca55c922beb88db971ba3a9a42da80
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 15:13:35 +0300

    add login components to landing and grab user inputs on landing screen

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let [phone, setPhone] = useState("");
-  let [amount, setAmount] = useState("");
+  
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -37,23 +36,7 @@ function App() {
 
   return (
     <main>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="number"
-          placeholder="Phone"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          required
-        />
-        <input
-          type="number"
-          placeholder="Total"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-          required
-        />
-        <button type="submit">Submit</button>
-      </form>
+
     </main>
   );
 }
---
+++ src/Landing.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Landing() {
+  let [phone, setPhone] = useState("");
+  let [amount, setAmount] = useState("");
+  return (
+    <form>
+      <input
+        type="number"
+        placeholder="Phone"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        required
+      />
+      <input
+        type="number"
+        placeholder="Total"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        required
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
