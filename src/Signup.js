763db24094213e80088c39f6f742406c88fbafca
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 16:55:04 +0300

    add usestates for signup page

--- src/Signup.js
+++ src/Signup.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 export default function Signup() {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [confirm, setConfirm] = useState("");
+
   return (
     <main>
       <form>
-        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Email" value={email} />
         <input type="text" placeholder="Enter Password" />
         <input type="text" placeholder="Confirm Password" />
         <button type="submit">Submit</button>
