daf000eccfe7f85410f69868a3d047816cbc24db
Enock <enock.mokua@student.moringaschool.com>
2023-01-03 00:14:53 +0300

    add total input and gra inputs

--- src/Main.js
+++ src/Main.js
@@ -2,11 +2,21 @@ import { useState } from "react";
 
 export default function Main() {
   let [phone, setPhone] = useState("");
+  let [total, setTotal] = useState("");
 
   return (
     <main>
       <from>
-        <input type="number" value={phone} on />
+        <input
+          type="number"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <input
+          type="number"
+          value={total}
+          onChange={(e) => setTotal(e.target.value)}
+        />
       </from>
     </main>
   );
